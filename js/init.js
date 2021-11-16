0a5ee4c9decfd5b1403ea27ffdfe415152420e9c
pwebApp <pwebApp11@gmail.com>
2021-11-16 13:37:54 -0600

    Config

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyDIqkeVsJFLEz0isHlWBbjRE1DM-_iHFVI",
   authDomain: "fbaut-9cbf4.firebaseapp.com",
   projectId: "fbaut-9cbf4",
   storageBucket: "fbaut-9cbf4.appspot.com",
   messagingSenderId: "348420763750",
   appId: "1:348420763750:web:ab380a0c3e31a91b813da7"
-    });
+};
